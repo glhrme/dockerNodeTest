a43352cfb4cd4eb99454720aeac3bd133958dfb5
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-28 00:26:58 -0300

    Novas configs com docker e dotenv

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM node:latest
 LABEL Guilherme dos Santos
-ENV PORT=3000
 COPY . /var/www
 WORKDIR /var/www
 RUN yarn
 ENTRYPOINT yarn build
-EXPOSE "8080:3000"
+EXPOSE 3000 "80:3000"
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: process.env.TS_NODE_DEV ? '.env.development' : '.env'
+})
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Oi'))
 
-app.listen(process.env.PORT, () => console.log(process.env.PORT))
+app.listen(process.env.PORT, () => console.log(process.env.PORT, process.env.TS_NODE_DEV))
